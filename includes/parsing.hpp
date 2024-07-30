e2b58ea2c3b61f4674534f37cff7e9c9b01412de
Pierre ANGER <waticouzz@gmail.com>
2024-07-30 14:40:13 +0200

    Server renamed to Socket

--- includes/parsing.hpp
+++ includes/parsing.hpp
@@ -6,20 +6,20 @@
 /*   By: panger <panger@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 14:50:31 by panger            #+#    #+#             */
-/*   Updated: 2024/07/29 14:32:50 by panger           ###   ########.fr       */
+/*   Updated: 2024/07/30 14:39:28 by panger           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
 # define PARSING_H
 
-#include "Server.hpp"
+#include "Socket.hpp"
 #include "enums.hpp"
 #include <iostream>
 #include <fstream>
 #include "Request.hpp"
 
-Server	*parseConfig(std::string conf_path);
+Socket	*parseConfig(std::string conf_path);
 void	parseRequestLine(std::string r, Request &request);
 void	parseHeaders(std::string r, Request &req);
 
